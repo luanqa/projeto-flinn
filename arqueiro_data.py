609350b7221109c33b836a1d0152159a11936df5
luanqa <lnsantos482@gmail.com>
2024-09-09 16:47:19 -0300

    Alterando os inimigos e adicionando possivel ideia para fazer a inserção de caminhos diferentes para a mesma fase

---
+++ arqueiro_data.py
@@ -0,0 +1,22 @@
+TURRET_DATA = [
+  {
+    #1
+    "range": 100,
+    "cooldown": 1000,
+  },
+  {
+    #2
+    "range": 120,
+    "cooldown": 900,
+  },
+  {
+    #3
+    "range": 135,
+    "cooldown": 700,
+  },
+  {
+    #4
+    "range": 160,
+    "cooldown": 600,
+  }
+]
---
+++ baloes_data.py
@@ -0,0 +1,69 @@
+ENEMY_SPAWN_DATA = [
+  {
+    #1 mesmo balao pra um atirador
+    "weak": 4,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #2 mais de um tipo de balao para varios atiradores
+    "weak": 1,
+    "medium": 1,
+    "strong": 1,
+    "elite": 1
+  },
+  {
+    #3 mesmo balao para varios atiradores
+    "weak": 8,
+    "medium": 0,
+    "strong": 0,
+    "elite": 0
+  },
+  {
+    #4 varios para varios
+    "weak": 2,
+    "medium": 2,
+    "strong": 2,
+    "elite": 2
+  }
+]
+
+ENEMY_DATA = {
+    "weak": {
+    "health": 10,
+    "speed": 1
+  },
+    "medium": {
+    "health": 10,
+    "speed": 2
+  },
+    "strong": {
+    "health": 10,
+    "speed": 3
+  },
+    "elite": {
+    "health": 10,
+    "speed": 4
+  }
+}
+'''
+ENEMY_DATA = {
+    "weak": {
+    "health": 10,
+    "speed": 1
+  },
+    "medium": {
+    "health": 15,
+    "speed": 2
+  },
+    "strong": {
+    "health": 20,
+    "speed": 3
+  },
+    "elite": {
+    "health": 30,
+    "speed": 4
+  }
+}
+'''
